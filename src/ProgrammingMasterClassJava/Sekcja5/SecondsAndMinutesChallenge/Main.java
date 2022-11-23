3c030500167f8eddbaf5dcc216ad9a112c5eea4c
kadyj5 <karolinadyjak@gmail.com>
2022-11-23 21:21:02 +0100

    final commit

--- src/ProgrammingMasterClassJava/Sekcja5/SecondsAndMinutesChallenge/Main.java
+++ src/ProgrammingMasterClassJava/Sekcja5/SecondsAndMinutesChallenge/Main.java
@@ -2,6 +2,18 @@ package ProgrammingMasterClassJava.Sekcja5.SecondsAndMinutesChallenge;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getDurationString(36500));
+    }
 
+    public static String getDurationString(int seconds) {
+        int minutes = seconds / 60;
+        if (minutes != 0) seconds = seconds - (minutes * 60);
+        return (getDurationString(minutes, seconds));
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        int hours = minutes / 60;
+        if (hours != 0) minutes = minutes - (hours * 60);
+        return ("" + hours + "h " + minutes + "m " + seconds + "s");
     }
 }
